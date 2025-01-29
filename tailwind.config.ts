984eacf4f2ded92faded50fd0e1655bd769c975a
Mboya72 <elvindio72@gmail.com>
2025-01-29 23:28:33 +0300

    importing somethings correctly

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,24 @@ export default {
   ],
   theme: {
     extend: {
+      backdropBlur: {
+        sm: '4px',
+        DEFAULT: '8px',
+        md: '12px',
+        lg: '16px',
+        xl: '24px',
+      },
+      lineHeight: {
+        'extra-tight': '1', // Custom line height
+      },
+      fontSize: {
+        '10xl': '10rem',   // 160px
+        '11xl': '12rem',   // 192px
+        '12xl': '14rem',   // 224px
+      },
+      fontFamily: {
+        afacad: ['Afacad', 'sans-serif'], // Adding Afacad font family to Tailwind
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
